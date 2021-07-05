81ea007cac786f43b312db7373e83f526aacdf83
WYang6060 <wyang6060@outlook.com>
2021-07-05 19:25:31 +0200

    init frontend

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+import TotalCompletedItems from './components/TotalCompletedItems';;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container bg-white p-4 mt-5'>
+      <h1>React Redux Todos App</h1>
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompletedItems />
     </div>
-  );
+  );  
 }
 
 export default App;
